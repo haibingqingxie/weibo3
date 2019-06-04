9b1c29e38f11084eb80df7cce0c2f8d41c2f07c4
haibingqingxie <jasonli@seasidecrab.com>
2019-06-04 08:57:27 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/6/4
+ * Time: 16:51
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/6/4
+ * Time: 16:47
+ */
+?>
+@extends('layouts.default')
+@section('content')
+    <h1>关于页</h1>
+@stop
