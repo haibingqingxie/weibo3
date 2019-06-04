200889070de089dba78e1eca395ef9921c7e91c8
haibingqingxie <jasonli@seasidecrab.com>
2019-06-04 10:10:48 +0000

    用户注册页面

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/6/4
+ * Time: 18:09
+ */
+?>
+@extends('layouts.default')
+@section('title', '注册')
+
+@section('content')
+    <h1>注册</h1>
+@stop
